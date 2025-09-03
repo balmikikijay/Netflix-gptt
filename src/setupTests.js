51e8ce2ab89bd929ff838520c4f42d80516ee583
balmikikijay <kbalmiki813@gmail.com>
2025-09-03 16:11:25 +0530

    netflix-gpt

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+ const hey = () => { 
+    return(
+        console.log("heybro how ar you i'm just telling you how ar ou so that you can recognize me")
+    )
+}
